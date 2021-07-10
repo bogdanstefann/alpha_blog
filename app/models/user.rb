e2a84f828a87780a7efe2956bb4d72ce039f1c74
bogdanstefann <bogdanstefann@gmail.com>
2021-07-10 16:57:51 +0300

    Added password_digest for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
 											length: { maximum: 105 },
 											format: { with: VALID_EMAIL_REGEX }
 	before_save { self.email = email.downcase }
+	has_secure_password
 end
