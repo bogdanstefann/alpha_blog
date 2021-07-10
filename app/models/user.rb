fb76cb86fec0961eafe05f0f4476d846a5d67cb0
bogdanstefann <bogdanstefann@gmail.com>
2021-07-10 16:41:11 +0300

    Add before_save method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
 	has_many :articles
-	
 	validates :username, presence: true, 
 											uniqueness: { case_sensitive: false },
 											length: { minimum: 3, maximum: 25 }
@@ -9,4 +8,5 @@ class User < ApplicationRecord
 											uniqueness: true,
 											length: { maximum: 105 },
 											format: { with: VALID_EMAIL_REGEX }
+	before_save { self.email = email.downcase }
 end
